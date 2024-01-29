28e15570141889bcb7b0b204c19691c7acd25a12
Ryan Stradling <rstradling@insiderealestate.com>
2024-01-29 11:32:09 -0500

    Making host and port be hardcoded for not.  The host hard coding is not that bad as it uses kubernetes networking dns for it

--- src/main/scala/com/strad/repositories/Db.scala
+++ src/main/scala/com/strad/repositories/Db.scala
@@ -7,21 +7,19 @@ import doobie.implicits.*
 import doobie.hikari.*
 
 object Db:
-  case class DbConfig(driverName: String, db: String, host: String, port: Int, user: String, password: String)
+  case class DbConfig(driverName: String, db: String, user: String, password: String)
   def mkConnection[F[_]](
                         dbConfig: DbConfig
                         )(using e : Async[F]): Resource[F, Transactor[F]] =
     for
       ce <- ExecutionContexts.fixedThreadPool[F](32)
-      url = s"jdbc:postgresql://${dbConfig.host}:${dbConfig.port}/${dbConfig.db}"
+      url = s"jdbc:postgresql://postgres:5432/${dbConfig.db}"
       xa <- HikariTransactor.newHikariTransactor[F](dbConfig.driverName, url, dbConfig.user, dbConfig.password, ce)
     yield xa
   def mkDbConfigFromEnv(driverName: String): DbConfig =
     val db = sys.env.getOrElse("POSTGRES_DB", throw new RuntimeException("Must specify POSTGRES_DB env var"))
     val pass = sys.env.getOrElse("POSTGRES_PASSWORD", throw new RuntimeException("Must specify POSTGRES_PASSWORD env var"))
     val user = sys.env.getOrElse("POSTGRES_USER", throw new RuntimeException("Must specify POSTGRES_USER env var"))
-    val host = sys.env.getOrElse("POSTGRES_HOST", throw new RuntimeException("Must specify POSTGRES_HOST env var"))
-    val port = sys.env.getOrElse("POSTGRES_PORT", throw new RuntimeException("Must specify POSTGRES_PORT env var"))
-    DbConfig(driverName, db, host, port.toInt, user, pass)
+    DbConfig(driverName, db, user, pass)
 
 
