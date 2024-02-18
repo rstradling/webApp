ea4fd4e080b0bb5c7db947714d239f2f680b8c78
rstradling <rstradling@insiderealestate.com>
2024-02-18 17:57:31 -0500

    Rearranging the code so that the tests work.  Fixing things up

--- src/main/scala/com/strad/service/ModelsJson.scala
+++ src/main/scala/com/strad/service/ModelsJson.scala
@@ -1,21 +1,22 @@
 package com.strad.service
 
 import cats.effect.Concurrent
-import cats.implicits._
-import io.circe.{Encoder, Decoder}
+import cats.implicits.*
+import io.circe.{Decoder, Encoder}
 import com.strad.service.domain.*
-import org.http4s._
-import org.http4s.implicits._
+import org.http4s.*
+import org.http4s.implicits.*
 import org.http4s.client.Client
 import org.http4s.client.dsl.Http4sClientDsl
-import org.http4s.circe._
-import org.http4s.Method._
+import org.http4s.circe.*
+import org.http4s.Method.*
 
 object ModelsJson:
   given Decoder[UserRequest] = Decoder.derived[UserRequest]
 
-  given [F[_] : Concurrent]: EntityDecoder[F, UserRequest] = jsonOf
+  given [F[_]: Concurrent]: EntityDecoder[F, UserRequest] = jsonOf
 
   given Encoder[UserResponse] = Encoder.AsObject.derived[UserResponse]
+  given Decoder[UserResponse] = Decoder.derived[UserResponse]
 
   given [F[_]]: EntityEncoder[F, UserResponse] = jsonEncoderOf
---
+++ src/test/scala/com/strad/service/ServiceRoutesSpec.scala
@@ -0,0 +1,62 @@
+package com.strad.service
+
+import cats.*
+import cats.effect.*
+import cats.implicits.*
+import com.strad.ServiceRoutes
+import com.strad.repositories.UserRepo
+import com.strad.repositories.domain.User
+import com.strad.service.ModelsJson.given
+import com.strad.service.domain.UserResponse
+import io.circe.*
+import io.circe.generic.auto.*
+import io.circe.syntax.*
+import munit.CatsEffectSuite
+import org.http4s.*
+import org.http4s.circe.*
+import org.http4s.circe.CirceEntityDecoder.circeEntityDecoder
+import org.http4s.client.Client
+import org.http4s.dsl.io.*
+import org.http4s.implicits.uri
+
+class ServiceRoutesSpec extends CatsEffectSuite:
+  private val success: UserRepo[IO] = new UserRepo[IO]:
+    override def getUserById(id: Long): IO[Option[User]] = IO.pure(Some(User(123, None)))
+    override def insertUser(user: User): IO[User] = ???
+    override def deleteUserById(userId: Long): IO[Unit] = ???
+
+  private val foundNone: UserRepo[IO] = new UserRepo[IO]:
+    override def getUserById(id: Long): IO[Option[User]] = IO.pure(None)
+    override def insertUser(user: User): IO[User] = ???
+    override def deleteUserById(userId: Long): IO[Unit] = ???
+
+  private val exception: UserRepo[IO] = new UserRepo[IO]:
+    override def getUserById(id: Long): IO[Option[User]] = IO.raiseError(new RuntimeException("Should not get called!"))
+    override def insertUser(user: User): IO[User] = ???
+    override def deleteUserById(userId: Long): IO[Unit] = ???
+
+  private def userServiceRoutes(repo: UserRepo[IO]) =
+    ServiceRoutes.userRoutes[IO](new UserServiceImpl[IO](repo)).orNotFound
+
+  test("GET /api/v1/users/123 should return 200 OK with the correct response"):
+    val expectedResponse = UserResponse(123, None)
+    val request: Request[IO] = Request(method = Method.GET, uri = uri"/api/v1/users/123")
+    val client = Client.fromHttpApp(userServiceRoutes(success))
+    val status = client.status(request)
+    assertIO(status, Ok)
+    val resp = client.expect[UserResponse](request)
+    assertIO(resp, expectedResponse)
+
+  test("GET /api/v1/users/123 should return 404 when not found"):
+    val request: Request[IO] = Request(method = Method.GET, uri = uri"/api/v1/users/123")
+    val client = Client.fromHttpApp(userServiceRoutes(foundNone))
+    val status = client.status(request)
+    assertIO(status, NotFound)
+
+/*  test("GET /api/v1/users/123 should return 500 when an exception is thrown"):
+    val request: Request[IO] = Request(method = Method.GET, uri = uri"/api/v1/users/123")
+    val client = Client.fromHttpApp(userServiceRoutes(exception))
+    val status = client.status(request)
+    assertIO(status, NotFound)
+
+ */
